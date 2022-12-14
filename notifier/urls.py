68289ce7f624a4f17119c2c8006c86cd70fcc06e
Aris Miletis <arismiletis@outlook.com>
2022-12-14 21:31:25 +0200

    notifier

--- notifier/urls.py
+++ notifier/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from notifier.viber.views import message
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", message),
 ]
--- notifier/viber/views.py
+++ notifier/viber/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(["POST"])
+def message(request):
+    if request.method == "POST":
+        if request.data["message"]["text"]:
+            requests.post(
+                url="https://chatapi.viber.com/pa/send_message",
+                json={
+                    "receiver": "Y/WGvosl2THqAOzX8ZSKOg==",
+                    "sender": {"name": "Bus Helper"},
+                    "tracking_data": "tracking data",
+                    "type": "text",
+                    "text": request.data["message"]["text"],
+                },
+                headers={
+                    "X-Viber-Auth-Token": "502ffbbd70a7e53f-7c2fccec81219468-5d3130e602ebb845"
+                },
+                timeout=20,
+            )
+        return Response({})
